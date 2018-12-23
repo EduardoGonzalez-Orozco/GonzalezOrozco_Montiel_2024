be329bba850ca6d2ef41c9ba16f55cdce6a14fbf
eduardo1992 <eduardo.glez.or@gmail.com>
2018-12-23 11:42:15 -0600

    Add files via upload

---
+++ genotype_input_generator_GoodMK.NoPhased.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl;
+
+### Program to change Genotype conde (0|0)(1|1) to alte/ref, and outputting in a matrix horizontal
+## Usage
+
+#perl genotype_input_generator.pl vcf_file.vcf
+
+# NOTA: para Mitcondrial se asumira homocigoto ¡¡¡¡ (con fines de calulode LD) 
+
+
+
+#use strict;
+use warnings;
+
+$file = $ARGV[0];
+
+$outF="matrix.$file.NoPhased.mtr";
+
+open (OUT, "+>$outF") or die $!;
+
+open(FF,$file);
+
+$count1=0;
+
+while(<FF>) {
+
+	$li=$_;
+	
+	if($li =~ /^[#]/) { next;}
+	
+	@line =split("\t",$li);
+	
+	$totalId=$#line-8;
+	chomp @line;	
+	## Resta 8 posiciones a el total de elementos, ahí empieza los indiviudos
+	$chr=$line[0];
+	$pos=$line[1];
+	$ref=$line[3];
+	$alt=$line[4];
+
+	$arraout[0]="CP.$chr.$pos\t";
+
+	##### Generating array 
+
+	for($x=9; $x < $#line+1; ++$x) {
+					$count2=$x-8;
+					#print "VOY $x\n";	
+					#print "REF $ref ALT $alt\n";
+					#print "LL <$line[$x]>\n";
+					if ($line[$x] eq "0|0") { $arraout[$count2]="$ref/$ref\t";}
+					if ($line[$x] eq "0|1") { $arraout[$count2]="$ref/$alt\t";}
+ 					if ($line[$x] eq "1|0") { $arraout[$count2]="$ref/$alt\t";}
+					if ($line[$x] eq "1|1") { $arraout[$count2]="$alt/$alt\t";}
+					if($line[$x] eq "0") 	{$arraout[$count2]="$ref\t";}
+					if($line[$x] eq "1")    {$arraout[$count2]="$alt\t";}
+					#print "OO $arraout[$count2]\n";
+
+					 #<STDIN
+			}
+
+	print OUT "@arraout\n"; 
+
+}
+
+
+close OUT;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test_GK.r
@@ -0,0 +1,34 @@
+
+### Código con librerías para probar el estadìstico tau de Goodman-Kruskal
+
+
+#Librerías:
+library(DescTools)
+library(genetics)
+
+# Input genotipos 
+g1 <-  genotype( c("T/T","A/A","A/A","A/A","A/A","A/A","A/A","A/A","A/A","T/T","A/A","A/A","A/A","A/A","A/A","A/A","A/A","A/A","A/A","A/A"))
+g2 <-  genotype( c("G/C","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/C","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/G"))
+
+#Estimación utilizando estadísticos clásicos 
+lin <-LD(g1,g2)
+
+##Output de resultados
+lin$`R^2`
+lin$`D'`
+lin$D
+
+
+##Input para tau de Goodman Kruskal
+gg1 <-  c("T/T","A/A","A/A","T/T","A/A","A/A","A/A","A/A","T/T","T/T","A/A","A/A","A/A","A/A","A/A","A/A","A/A","A/A","A/A","A/A")
+gg2 <-   c("G/C","G/G","G/G","G/C","G/G","G/G","G/G","G/G","G/C","G/C","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/G","G/G")
+
+# Estimaación utilizando Goodman_kruskal tau
+GoodmanKruskalTau(gg1,gg2,direction = "column")
+
+## Calculo y output de resultados
+GoodmanKruskalTau(gg1,gg2)
+
+
+
+
